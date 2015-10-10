5353ad7e28b7cead369bbf7587c9a37a0e110aa7
rabarry <rbarry3@gmail.com>
2015-10-10 00:54:34 -0700

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,3 +1,6 @@
+# Exploratory Data Analysis Project #1
+# Plot 3
+
 # Description: Measurements of electric power consumption in one household with a one-minute sampling rate over a period of almost 4 years. Different electrical quantities and some sub-metering values are available.
 #
 # The following descriptions of the 9 variables in the dataset are taken from the UCI web site:
